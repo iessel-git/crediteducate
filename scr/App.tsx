ac161030f74ef0f1b9badac8b64fe98521323f32
iessel-git <isaac@csetechsolution.com>
2025-11-23 17:01:55 -0500

    Update App.tsx

--- scr/App.tsx
+++ scr/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Dashboard from './screens/Dashboard';
+import Dashboard from './screens/Dashboard/Dashboard';
 import Accounts from './screens/Accounts';
 import Scenarios from './screens/Scenarios';
 import Learn from './screens/Learn';
