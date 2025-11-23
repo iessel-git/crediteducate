ff88edbf86e11d11d8dc6439e869333ae8da25dd
iessel-git <isaac@csetechsolution.com>
2025-11-23 16:53:59 -0500

    Update Account.ts

--- scr/models/Account.ts
+++ scr/models/Account.ts
@@ -1,1 +1,12 @@
+// File: src/models/Account.ts
+export type AccountType = 'credit' | 'installment' | 'mortgage';
 
+export interface Account {
+  id: string;
+  name: string;
+  type: AccountType;
+  limit?: number; // for revolving
+  balance: number;
+  openedYears?: number;
+  missedPayments?: number;
+}
