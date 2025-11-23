59e3eb5432f3078093b85fe1029a066408f201fa
iessel-git <isaac@csetechsolution.com>
2025-11-23 18:06:44 -0500

    Update App.tsx

--- scr/App.tsx
+++ scr/App.tsx
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Dashboard from './screens/Dashboard/Dashboard';
 import Accounts from './screens/Accounts/Accounts';
 import Scenarios from './screens/Scenarios/Scenarios';
-import Learn from './screens/Learn';
+import Learn from './screens/Learn/Learn';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 
